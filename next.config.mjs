98c44098ed6559fed8e71a25d9f9889332091b5e
jpautasso <juan.pautasso@pjn.gov.ar>
2025-04-02 15:43:26 -0300

    Nuevo código para reemplazar el proyecto anterior-8

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
   // env: {
   //   NEXT_PUBLIC_VARIABLE_NAME: process.env.NEXT_PUBLIC_VARIABLE_NAME,
   // },
+  
+  // Añadiendo esta configuración para ayudar con problemas de rutas
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Estas reglas se evalúan antes de que Next.js busque archivos en el sistema
+        {
+          source: '/',
+          destination: '/app/page',
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig; 
